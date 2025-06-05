fd4ba2948dd5fe5ffa9cd60cf41817a07188bf2c
HeitorDalla <heitorvillavilla@gmail.com>
2025-06-05 10:12:44 -0300

    Arquivos criados. HTML faltando o container que vai receber a resposta da api, css zerado e javascript começando a adicionar o eventod e click

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const cnpjValue = document.querySelector("#")
+
+const url = `https://www.receitaws.com.br/v1/cnpj/${cnpj}`;
+
+document.querySelector("#btnBuscar").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const
+})
