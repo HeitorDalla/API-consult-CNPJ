03376634c425c06b9d0355e72bd4fbe24487faf5
HeitorDalla <heitorvillavilla@gmail.com>
2025-06-05 10:28:08 -0300

    Feita a parte de requisicao da api

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 "use strict";
 
-const cnpjValue = document.querySelector("#")
-
-const url = `https://www.receitaws.com.br/v1/cnpj/${cnpj}`;
-
-document.querySelector("#btnBuscar").addEventListener("click", (e) => {
+document.querySelector("#btnBuscar").addEventListener("click", async (e) => {
     e.preventDefault();
 
-    const
+    const respostaAPI = document.querySelector("#respostaAPI");
+    const cnpj = document.querySelector("#cnpj").value;
+
+    const url = `https://www.receitaws.com.br/v1/cnpj/${cnpj}`;
+
+    const dados = requisicaoAPI(url);
+
+    popularDados(dados, respostaAPI);
 })
+
+async function requisicaoAPI (url) {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Não possível carregar seus dados.")
+        }
+
+        return response.json();
+    } catch (error){
+        console.error(error);
+        return;
+    }
+}
+
+function popularDados (dados, resposta){
+    console.log(dados)
+    console.log(dados)
+}
